2ac6ee11033704fceadd841d464d1c928bedd43d
Hubert Muller <hubert@sportport.eu>
2022-04-24 15:28:27 +0200

    mock server

---
+++ src/mock.js
@@ -0,0 +1,28 @@
+const http = require('http');
+
+const server = http.createServer( (_req, res) => {
+
+    console.log('przyszlo zapytanie');
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'application/json');
+
+    res.end(`
+    [
+        {
+            "id": 1,
+            "imie": "hubert",
+            "nazwisko": "nowak"
+        },
+        {
+            "id": 2,
+            "imie": "adam",
+            "nazwisko": "kowalski"
+        }
+    ]
+    `);
+} );
+
+server.listen(3002, '127.0.0.1', () => {
+    console.log('serwer wystartowal');
+})
+
