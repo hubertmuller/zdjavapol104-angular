5ca4c8b5e735908faef547c801edd50a1b553ff0
Hubert Muller <hubert@sportport.eu>
2022-04-24 13:43:22 +0200

    stylowanie

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class AppComponent {
   public title = 'projekt';
+  
+  @Input()
+  public imie: string = 'cwqc';
+
   constructor() {
     this.title += ' 1';
   }
+
   public dodaj(a: number,b: number):number {
     return a + b;
   }
---
+++ src/app/menu/menu.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnDestroy, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+  selector: 'app-menu',
+  templateUrl: './menu.component.html',
+  styleUrls: ['./menu.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated
+})
+export class MenuComponent implements OnInit, OnDestroy {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
+}
